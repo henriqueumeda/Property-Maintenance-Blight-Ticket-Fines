b7781a67f74042961b9b343bbda3b62b9af5ff6f
Henrique Umeda <issamuumeda@gmail.com>
2021-09-16 14:27:11 -0300

    Save model

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def transform(le, data_list):
 #função para treinar o modelo
 def train_model():
     data = get_data()
-    columns = ['disposition', 'violation_code', 'judgment_amount']
+    columns = ['disposition', 'violation_code', 'owed_amount']
     X = data[columns]
     y = data['compliance']
     X['disposition'] = transform(le_disposition, X['disposition'])
@@ -44,7 +44,7 @@ def train_model():
 data = get_data()
 
 #treinando o modelo
-columns = ['disposition', 'violation_code', 'judgment_amount']
+columns = ['disposition', 'violation_code', 'owed_amount']
 X = data[columns]
 y = data['compliance']
 le_disposition = LabelEncoder().fit(list(X['disposition']) + ['Unknown'])
@@ -61,7 +61,7 @@ st.markdown("This is a Data App used to show the Machine Learning solution for t
 st.subheader("Selecting the attributes")
 
 #atributos para serem exibidos por padrão
-defaultcols = ['disposition', 'violation_code', 'violation_description', 'judgment_amount']
+defaultcols = ['disposition', 'violation_code', 'violation_description', 'owed_amount']
 
 #definindo atributos a partir do multiselect
 cols = st.multiselect("Attributes", data.columns.tolist(), default=defaultcols)
@@ -72,14 +72,14 @@ st.dataframe(data[cols].head(10))
 st.subheader("Distribution of blight tickets by sum of all fines and fees")
 
 #definindo a faixa de valores
-faixa_valores = st.slider("Judgment Amount", float(data.judgment_amount.min()), 1000., (0.0, 350.0))
+faixa_valores = st.slider("Owed Amount", float(data.owed_amount.min()), 1000., (0.0, 350.0))
 
 #filtrando os dados
-dados = data[data['judgment_amount'].between(left=faixa_valores[0], right=faixa_valores[1])]
+dados = data[data['owed_amount'].between(left=faixa_valores[0], right=faixa_valores[1])]
 
 #plot a distribuição dos dados
-f = px.histogram(dados, x="judgment_amount", nbins=100, title="Distribution of Judgment Amount")
-f.update_xaxes(title="Judgment Amount")
+f = px.histogram(dados, x="owed_amount", nbins=100, title="Distribution of Owed Amount")
+f.update_xaxes(title="Owed Amount")
 f.update_yaxes(title="Total People")
 st.plotly_chart(f)
 
@@ -99,19 +99,20 @@ violation = st.sidebar.selectbox('Violation', data.loc[data['violation_descripti
 violation = transform(le_violation, pd.Series(violation))[0]
 
 fine = st.sidebar.number_input("Fine Amount", value=data.fine_amount.mean())
-admin = st.sidebar.number_input("Admin Fee", value=0)
-state = st.sidebar.number_input("State Fee", value=0)
-late = st.sidebar.number_input("Late Fee", value=0)
-judgment_amount = fine + admin + state + late
-st.sidebar.text("Judgment Amount")
-st.sidebar.text(judgment_amount)
+admin = st.sidebar.selectbox("Admin Fee", (0, 20))
+state = st.sidebar.selectbox("State Fee", (0, 10))
+late = st.sidebar.selectbox("Late Fee", (0, fine*0.1))
+discount = st.sidebar.number_input("Discount Amount", value=0)
+owed_amount = fine + admin + state + late - discount
+st.sidebar.text("Owed Amount")
+st.sidebar.text(owed_amount)
 
 #inserindo um botão na tela
 btn_predict = st.sidebar.button("Predict")
 
 #verifica se o botão foi acionado
 if btn_predict:
-    result = model.predict_proba([[disposition, violation, judgment_amount]])
+    result = model.predict_proba([[disposition, violation, owed_amount]])
     st.subheader("The chance of this person paying the blight ticket on time is:")
     result = str(round(result[0][1]*100,2)) + '%'
     st.write(result)
